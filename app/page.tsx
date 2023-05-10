b324337973fb9540eb2dd467cb1c9bd11296e778
inzaky <inzaky2000@yahoo.com>
2023-05-10 22:03:51 +0700

    add thai text

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
               height={24}
               priority
             />
-          </a>
+           </a>
         </div>
       </div>
 
@@ -105,6 +105,7 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
+            ทดลองก่อน
           </p>
         </a>
       </div>
